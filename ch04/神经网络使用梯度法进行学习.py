0199237b2c704e3d0dbe7f489d638bd10fb139b8
lijianhao <haohaos_123@163.com>
2023-02-21 14:28:53 +0800

    — 二层神经网络使用梯度法深度学习的实现

--- ch04/神经网络使用梯度法进行学习.py
+++ ch04/神经网络使用梯度法进行学习.py
@@ -1,13 +1,16 @@
+from numpy import gradient
+
 from common.functions import *
 from common.gradient import numerical_gradient
 
 
+# 定义二层神经网络学习的类
 class TwoLayerNet:
     """参数：输入层神经元数、隐藏层神经元数、输出层神经元数"""
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         # 初始化权重
-        self.prarms = {
+        self.params = {
             'W1': weight_init_std * np.random.randn(input_size, hidden_size),  # 第一层权重
             'b1': np.zeros(hidden_size),  # 第一层偏置
             'W2': weight_init_std * np.random.randn(hidden_size, output_size),  # 第二层权重
@@ -16,8 +19,8 @@ class TwoLayerNet:
 
     def predict(self, x):
         """进行推理"""
-        W1, W2 = self.prarms['W1'], self.prarms['W2']
-        b1, b2 = self.prarms['b1'], self.prarms['b2']
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
 
         a1 = np.dot(x, W1) + b1
         z1 = sigmoid(a1)
@@ -47,20 +50,95 @@ class TwoLayerNet:
 
         # 各层权重偏置的梯度
         grads = {
-            'W1': numerical_gradient(loss_W, self.prarms['W1']),
-            'b1': numerical_gradient(loss_W, self.prarms['b1']),
-            'W2': numerical_gradient(loss_W, self.prarms['W2']),
-            'b2': numerical_gradient(loss_W, self.prarms['b2'])
+            'W1': numerical_gradient(loss_W, self.params['W1']),
+            'b1': numerical_gradient(loss_W, self.params['b1']),
+            'W2': numerical_gradient(loss_W, self.params['W2']),
+            'b2': numerical_gradient(loss_W, self.params['b2'])
         }
+
+        return grads
+
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
 
         return grads
 
 
 # params、grads字典数据示例
-net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
-# print(net.prarms['W1'].shape, net.prarms['b1'].shape, net.prarms['W2'].shape, net.prarms['b2'].shape)
+# net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+# print(net.params['W1'].shape, net.params['b1'].shape, net.params['W2'].shape, net.params['b2'].shape)
 
-x = np.random.rand(100, 784)
-t = np.random.rand(100, 10)
-grads = net.numerical_gradient(x, t)
-print(grads['W1'].shape, grads['b1'].shape, grads['W2'].shape, grads['b2'].shape)
+# x = np.random.rand(100, 784)
+# t = np.random.rand(100, 10)
+# grads = net.numerical_gradient(x, t)
+# print(grads['W1'].shape, grads['b1'].shape, grads['W2'].shape, grads['b2'].shape)
+
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+# 超参数
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+#################################
+train_acc_list = []
+test_acc_list = []
+# 平均每个epoch的重复次数
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    # 获取mini_batch,(从6W条数据随机抽取100条)
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 计算梯度
+    # grad = network.numerical_gradient(x_batch, t_batch)
+    grad = network.gradient(x_batch, t_batch)  # 高速版
+
+    # 更新参数
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # 记录学习过程
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    ##################################
+    # 计算每个epoch的识别精度
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+# print(train_loss_list)
